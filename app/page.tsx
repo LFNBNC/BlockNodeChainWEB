c9bca2a261aa839676cfea288827b924afb47f5e
Abin <abinbvinod@gmail.com>
2023-05-13 02:21:05 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
         <p className="fixed left-0 top-0 shadow hover:shadow-lg flex w-full justify-center bg-violet-900/30">
           Block Node Chain Under Development
         </p>
-       <div className='fixed content left-10'>
+       <div >
           <a
           href="https://lfnbnc.gitbook.io/blockchain/"
           className="Border">
-            <h1 className={'text-lg font-mono text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-violet-100 m-4 rounded-full bg-gradient-to-r from-violet-800 via-white-500 to-blue-500'}>
+            <h1 className={' fixed pl-6 text-indigo-400 group-hover:text-gray-100 transition duration-200'}>
                Documentation </h1>
            </a>
           </div>
@@ -52,6 +52,23 @@ export default function Home() {
         </a>
       </div>
       <h2 className='flex justify-center text-0x0 font-mono'>Github</h2>
+
+      <div class="px-8 py-32">
+  <div class="grid gap-8 items-start justify-center">
+    <div class="relative group">
+      <div class="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+      <button class="relative px-7 py-4 bg-black rounded-lg leading-none flex items-center divide-x divide-gray-600">
+        <span class="flex items-center space-x-5">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-pink-600 -rotate-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
+          </svg>
+          <span class="pr-6 text-gray-100">Block Node Chain</span>
+        </span>
+        <span class="pl-6 text-indigo-400 group-hover:text-gray-100 transition duration-200">Block Explorer &rarr;</span>
+      </button>
+    </div>
+  </div>
+</div>
     
     
   </main>
