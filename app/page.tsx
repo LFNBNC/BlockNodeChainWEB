513c81d24c5f6c86e73f9bfbbc4ed32cdcd593f2
Abin <abinbvinod@gmail.com>
2023-05-13 03:14:13 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
       
       <div className='relative flex items-center justify-center h-screen mb-12 overflow-hidden'>
        
-      <video className="relative z-60 p-5 absolute z-5 w-200 min-w-half min-h-half max-w-none"
+      <video className="absolute z-5 w-200 min-w-half min-h-half max-w-none"
       src={"/vd.mp4"}  
       width={1000} 
       height={1000} 
@@ -66,9 +66,10 @@ export default function Home() {
       <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
       <button className="relative px-7 py-4 bg-black rounded-lg leading-none flex items-center divide-x divide-gray-600">
         <span className="flex items-center space-x-5">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-pink-600 -rotate-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
-          </svg>
+         
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.412 15.655L9.75 21.75l3.745-4.012M9.257 13.5H3.75l2.659-2.849m2.048-2.194L14.25 2.25 12 10.5h8.25l-4.707 5.043M8.457 8.457L3 3m5.457 5.457l7.086 7.086m0 0L21 21" />
+</svg>
           <span className="pr-6 text-white-100 font-mono">POW x ZK</span>
         </span>
         <span className="font-mono pl-6 text-blue-400 group-hover:text-gray-100 transition duration-200">Block Explorer &rarr;</span>
@@ -81,6 +82,5 @@ export default function Home() {
   </main>
   )
 }
-
 
 
