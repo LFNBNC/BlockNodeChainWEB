f4ec53c6e26380a33a6ab52cf804d4b35b6e075e
Abin <abinbvinod@gmail.com>
2023-05-13 03:28:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
           href="https://github.com/L-F-N-BlockNodeChain"
           className="">
           <h1 className= "flex justify-center text-0x0 font-mono">
-          Block-Node-Chain</h1>
+          Block-Node-Chain &crarr;</h1>
           
         </a>
       </div>
@@ -70,6 +70,7 @@ export default function Home() {
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-violet-600 rotate-10">
   <path stroke-linecap="round" stroke-linejoin="round" d="M11.412 15.655L9.75 21.75l3.745-4.012M9.257 13.5H3.75l2.659-2.849m2.048-2.194L14.25 2.25 12 10.5h8.25l-4.707 5.043M8.457 8.457L3 3m5.457 5.457l7.086 7.086m0 0L21 21" />
 </svg>
+          
           <span className="pr-6 text-white-100 font-mono">PoW x ZkP </span>
         </span>
         <span className="font-mono pl-6 text-blue-400 group-hover:text-gray-100 transition duration-200">Block Explorer &rarr;</span>
