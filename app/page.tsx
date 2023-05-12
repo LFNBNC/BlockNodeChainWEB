ce9b737b9f2e96b3ab35360c8bd2185e0e60529c
Abin <abinbvinod@gmail.com>
2023-05-13 01:03:18 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
     <main className="flex min-h-screen flex-col items-right justify-between p-10">
       <div className="z-10 w-half max-w-5xl items-right justify-left font-mono text-sm lg:flex ">
-        <p className="fixed left-0 top-0 flex w-full justify-start border-b border-violet-400 bg-gradient-to-b from-violet-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-violet-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-violet-400 bg-violet-800/30">
           Block Node Chain&nbsp;
           <code className="font-sans font-sans"></code>
           Under Development
