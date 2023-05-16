5bc6fedbc3b827dac9acf1eb567852d0e59bbe2f
Abin <abinbvinod@gmail.com>
2023-05-17 01:19:51 +0530

    des

--- Component/bvid.tsx
+++ Component/bvid.tsx
@@ -5,7 +5,6 @@ const VideoBackground = () => {
       <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: -1 }}>
         <video autoPlay muted loop style={{ objectFit: 'cover', width: '100%', height: '100%' }}>
           <source src="/vd.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
         </video>
       </div>
     );
