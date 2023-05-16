27cf8c4eb38ff3e9fd17906aae0e434560c2b0e0
Abin <abinbvinod@gmail.com>
2023-05-17 02:01:02 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
 
       
       
-    <div className=" absolute  right-0 bottom-20 px-8 py-8">
+    <div className="fixed  absolute  right-0 bottom-20 px-8 py-8">
         <div className="grid gap-1 items-start justify-center">
           <div className="relative group">
             <div className="absolute -inset-0.5 bg-gradient-to-r from-blue-700 to-white-100 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
@@ -66,12 +66,12 @@ export default function Home() {
         <a
           href="https://github.com/L-F-N-BlockNodeChain"
           className="">
-          <h1 className="  absolute flex bottom-12 left-11 text-xm font-mono">
+          <h1 className=" fixed absolute flex bottom-12 left-11 text-xm font-mono">
             Block-Node-Chain &crarr;</h1>
 
         </a>
       </div>
-      <h2 className='  absolute flex bottom-8 left-11 text-xs font-mono'>Github</h2>
+      <h2 className=' fixed  absolute flex bottom-8 left-11 text-xs font-mono'>Github</h2>
 
 </main>
   )
