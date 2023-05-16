7de8df8f88f318650979573bf2ae8d6474af1356
Abin <abinbvinod@gmail.com>
2023-05-17 01:58:24 +0530

    tst

--- Component/bvid.tsx
+++ Component/bvid.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const VideoBackground = () => {
     return (
-      <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: -1 }}>
-        <video autoPlay muted loop style={{ objectFit: 'cover', width: '100%', height: '100%' }}>
+      <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: -1  }}>
+        <video  autoPlay muted loop style={{ objectFit: 'cover', width: '100%', height: '100%'  }}>
           <source src="/vdsm.mp4" type="video/mp4" />
         </video>
       </div>
