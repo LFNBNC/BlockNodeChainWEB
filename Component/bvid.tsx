0bdb745b526fab6a1548713701eec246c38691c6
Abin <abinbvinod@gmail.com>
2023-05-17 01:36:15 +0530

    web

--- Component/bvid.tsx
+++ Component/bvid.tsx
@@ -4,7 +4,7 @@ const VideoBackground = () => {
     return (
       <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: -1 }}>
         <video autoPlay muted loop style={{ objectFit: 'cover', width: '100%', height: '100%' }}>
-          <source src="/vd.mp4" type="video/mp4" />
+          <source src="/vdsm.mp4" type="video/mp4" />
         </video>
       </div>
     );
