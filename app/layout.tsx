d513e5599baf35b224333f460a3ebb2570876301
Abin <abinbvinod@gmail.com>
2023-05-20 00:51:04 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,6 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const Metadata = {
-    title : "BlockNodeChain",
-};
-
-
-
 export default function RootLayout({
   children,
 }: {
