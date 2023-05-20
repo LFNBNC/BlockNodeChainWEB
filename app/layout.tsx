09732006806c7c6d6f40a709fe72c9b06795ac75
Abin <abinbvinod@gmail.com>
2023-05-21 01:05:30 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <nav>
-        <h1 className="fixed left-0 top-0 shadow hover:shadow-lg flex w-full justify-center bg-violet-900/30">
+        <h1 className="fixed font-mono left-0 top-0 shadow hover:shadow-lg flex w-full justify-center bg-violet-900/30">
           Block Node Chain Under Development &#169;
          </h1>
         </nav>
