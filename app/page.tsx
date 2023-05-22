1dece052b2e049bdb2b32a54f0b84df9fce9e88d
Abin <abinbvinod@gmail.com>
2023-05-22 22:49:18 +0530

    web

--- app/page.tsx
+++ app/page.tsx
@@ -47,36 +47,49 @@ export default function Home() {
     </div>
       
      
-    <div className="absolute right-0 bottom-20 px-8 py-8">
-      <div className="grid gap-1 items-start justify-center">
-        <div className="relative group">
-          <div className="absolute -inset-0.5 bg-gradient-to-r from-blue-700 to-white-100 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-          <Link href="/network"> 
-            <button className="relative px-3 py-1 bg-black rounded-lg leading-none flex items-center divide-x divide-gray-600">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-violet-600 rotate-10">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M11.412 15.655L9.75 21.75l3.745-4.012M9.257 13.5H3.75l2.659-2.849m2.048-2.194L14.25 2.25 12 10.5h8.25l-4.707 5.043M8.457 8.457L3 3m5.457 5.457l7.086 7.086m0 0L21 21" />
-              </svg>
-  <h1 className="font-mono pl-6 text-blue-400 group-hover:text-gray-100 transition duration-200"> Block Node Chain Network &rarr; </h1>
-             </button>
-          </Link>
+    <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 sm:bottom-20 sm:w-1/2">
+        <div className="grid gap-1 items-start justify-center">
+          <div className="relative group">
+            <div className="absolute -inset-0.5 bg-gradient-to-r from-blue-700 to-white-100 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt animate-pulse" style={{ animationDuration: '4s' }}></div>
+            <Link href="/network">
+              <button className="relative px-2 py-2 bg-black rounded-lg leading-none flex items-center ">
+              <div className="relative w-8 h-8 animate-ping" style={{ animationDuration: '2s' }}>
+  <Image
+    src="/whitesvg.svg"
+    alt="Logo"
+    width={70}
+    height={70}
+    className="absolute top-0 left-0"
+  />
+  <Image
+    src="/svg.svg"
+    alt="Logo"
+    width={70}
+    height={70}
+    className="absolute top-1 left-0"
+  />
+</div>
+                <h1 className="font-mono text-base pl-6 text-violet-400 group-hover:text-gray-100 transition duration-500">
+                  Block Node Chain Network &rarr;
+                </h1>
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-     
       <div>
-        <a
-          href="https://github.com/L-F-N-BlockNodeChain"
-          className="">
-          <h1 className="  absolute flex bottom-12 left-11 text-xm font-mono">
-            Block-Node-Chain &crarr;</h1>
+  <a href="https://github.com/L-F-N-BlockNodeChain" className="">
+    <h1 className="fixed bottom-12 left-11 text-xs font-mono">
+      Block-Node-Chain &crarr;
+    </h1>
+  </a>
+</div>
 
-        </a>
-      </div>
-      
-      <h2 className='   absolute flex bottom-8 left-11 text-xs font-mono'>Github</h2>
-       <div className='   absolute flex bottom-8 left-24'>
-       <Image src="/github.svg" alt="Logo" width={15} height={15} />
-       </div>
+<h2 className="fixed bottom-8 left-11 text-xs font-mono">Github</h2>
+<div className="fixed bottom-8 left-24">
+  <img src="/github.svg" alt="Logo" width="15" height="15" />
+</div>
+
 </main>
   )
 }
