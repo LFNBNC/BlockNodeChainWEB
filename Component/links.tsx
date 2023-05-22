4b4bb8ac24f908018c8de0b9d64a918fc0214f9d
Abin <abinbvinod@gmail.com>
2023-05-23 02:33:11 +0530

    test

--- Component/links.tsx
+++ Component/links.tsx
@@ -9,18 +9,19 @@ const links = () => {
   
   
 return (
-    <div className="absolute bottom-0 left-0 right-0 text-center">
+    <div className="bg-gray-200 p-4 text-center fixed bottom-0 left-0 w-full flex justify-center">
     <div>
       <a href="https://github.com/L-F-N-BlockNodeChain" className="text-xs font-mono">
         Block-Node-Chain &crarr;
       </a>
     </div>
-    <h2 className="text-xs font-mono">Github</h2>
-    <div className="mt-1">
-      <img src="/github.svg" alt="Logo" width="15" height="15" className="inline-block ml-1" />
+    <h2 className="text-xs font-mono"></h2>
+    <div className="">
+      <img src="/github.svg" alt="Logo" width="10" height="10" className="inline-block ml-1" />
     </div>
   </div>
 
+  
   )
 }
 export default links;
