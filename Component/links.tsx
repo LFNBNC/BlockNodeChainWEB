6ad1298e7b84c90e30c7db067c370e87b1542d5a
Abin <abinbvinod@gmail.com>
2023-05-23 22:45:39 +0530

    Update links.tsx

--- Component/links.tsx
+++ Component/links.tsx
@@ -1,18 +1,16 @@
 const Footer: React.FC = () => {
     return (
-        
       <footer className='container flex justify-center transform-translate-x-1/5'>
       <div>
         <a href="https://github.com/L-F-N-BlockNodeChain" className="font-mono">
           Block-Node-Chain &crarr;
         </a>
-        <a className="ad">
-        <img src="/github.svg" width="15" height="15" />
+        <a className="absolute left-20">
+          <img src="/github.svg" width="10" height="10" />
         </a>
       </div>
     </footer>
     
-       
       
     );
   };
