4529b72b2ccbc28cfe4d0d7d257ec214961c94df
Abin <abinbvinod@gmail.com>
2023-06-19 21:27:37 +0530

    b

--- Component/grid.tsx
+++ Component/grid.tsx
@@ -5,10 +5,12 @@ import 'tailwindcss/tailwind.css';
 
 const Grid = () => {
  return (
-   <div>
+  <div className="container no-padding footer-note">
+    <div className="text-block-15">
      <hr className="my-10 border-gray-400 sm:mx-auto dark:border-gray-700 lg:my-7" />
-     <h1 className=''> </h1>
-   </div>
+  
+     </div>
+     </div>
 
  )
 }
