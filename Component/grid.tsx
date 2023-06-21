4e8ac961fcc716b3f025b01f68f2e3117ded63cb
Abin <abinbvinod@gmail.com>
2023-06-21 23:44:28 +0530

    Update grid.tsx

--- Component/grid.tsx
+++ Component/grid.tsx
@@ -5,8 +5,9 @@ import BentoGrid from '../Component/BentoGrid';
 const Grid = () => {
   const gridStyle: React.CSSProperties = {
     height: '40cm',
-    width: '20cm',
-    position: 'static'
+  
+    position: 'static',
+    background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 100, 0.5))',
   };
 
   return (
@@ -14,13 +15,13 @@ const Grid = () => {
       <hr className="my-10 border-gray-400 sm:mx-auto dark:border-gray-700 lg:my-7" />
       <div className="grid grid-cols-bento grid-rows-bento gap-4 md:w-1/2 mx-auto" style={gridStyle}>
         <BentoGrid>
-          <div className="bg-gray-500 hover:bg-gray-600 transition-colors duration-300 flex items-center justify-center text-white">
+          <div className="bg-white hover:bg-gray-200 transition-colors duration-300 flex items-center justify-center text-gray-800 rounded-lg p-4">
             Grid-1
           </div>
-          <div className="bg-gray-500 hover:bg-gray-600 transition-colors duration-300 flex items-center justify-center text-white">
+          <div className="bg-white hover:bg-gray-200 transition-colors duration-300 flex items-center justify-center text-gray-800 rounded-lg p-4">
             Grid-2
           </div>
-          <div className="bg-gray-500 hover:bg-gray-600 transition-colors duration-300 flex items-center justify-center text-white">
+          <div className="bg-white hover:bg-gray-200 transition-colors duration-300 flex items-center justify-center text-gray-800 rounded-lg p-4">
             Grid-3
           </div>
         </BentoGrid>
@@ -30,3 +31,5 @@ const Grid = () => {
 };
 
 export default Grid;
+
+
