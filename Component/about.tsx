4b18d3ba0124b447e8178f123bda2901b6c5d65c
Abin <abinbvinod@gmail.com>
2023-06-22 00:19:23 +0530

    Update about.tsx

--- Component/about.tsx
+++ Component/about.tsx
@@ -3,37 +3,45 @@ import 'tailwindcss/tailwind.css';
 
 const About = () => {
   const frameStyle: React.CSSProperties = {
-    bottom: '1rem',
-    right: '1rem',
+    alignItems: 'center',
+    background: 'linear-gradient(to right, rgba(0, 0, 255, 0.5), rgba(238, 130, 238, 0.5))',
+    padding: '1rem',
+  };
+
+  const textStyle: React.CSSProperties = {
+    fontFamily: 'monospace',
+    fontSize: '14px',
+    color: 'white',
+  };
+
+  const copyrightStyle: React.CSSProperties = {
+    ...textStyle,
     display: 'flex',
     justifyContent: 'flex-end',
     alignItems: 'center',
     background: 'linear-gradient(to right, rgba(0, 0, 255, 0.5), rgba(238, 130, 238, 0.5))',
     padding: '1rem',
-    borderRadius: '8px',
   };
 
   return (
-  
     <footer className="bg-violet dark:bg-gray-900">
-     
       <hr className="my-10 border-gray-400 sm:mx-auto dark:border-gray-700 lg:my-7" />
-      <div style={frameStyle}>
       <div className="container mx-auto">
-        
-       
-            <h1 className="font-mono flex justify-start">
-              <a href="https://github.com/L-F-N-BlockNodeChain" className="mr-4">
+        <div style={frameStyle}>
+          <div className="">
+            <h1 className="font-mono">
+              <a href="https://github.com/L-F-N-BlockNodeChain" className="mr-" style={textStyle}>
                 Github
               </a>
             </h1>
             <h3 className="font-mono">
-              <a href="https://www.linkedin.com/company/lfnbnc" className="ml-4">
+              <a href="https://www.linkedin.com/company/lfnbnc" className="ml-" style={textStyle}>
                 LinkedIn
               </a>
             </h3>
-          <div className="flex justify-center font-mono">© 2023 Block Node Chain™</div>
+          </div>
         </div>
+        <div style={copyrightStyle}>© 2023 Block Node Chain™</div>
       </div>
     </footer>
   );
