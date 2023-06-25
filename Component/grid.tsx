dd2881b6a663526be754714d70744950f354b760
Abin <abinbvinod@gmail.com>
2023-06-25 23:52:13 +0530

    Update grid.tsx

--- Component/grid.tsx
+++ Component/grid.tsx
@@ -3,27 +3,25 @@ import 'tailwindcss/tailwind.css';
 import BentoGrid from '../Component/BentoGrid';
 
 const Grid = () => {
+  const gridContainerStyle = 'grid-cols-3 gap-4 md:w-1/2 mx-auto';
+  const gridItemStyle = 'bg-white hover:bg-gray-200 transition-colors duration-300 flex items-center justify-center text-gray-800 rounded-lg p-4';
+  const gridContainerBgStyle = 'bg-gradient-to-b from-blue-100 to-blue-300';
+  const boxShadowStyle = 'shadow-md';
   const gridStyle: React.CSSProperties = {
-    height: '40cm',
-
-    position: 'static',
-    background: 'linear-gradient(to bottom, rgba(0, 0, 40, 0.2), rgba(0, 0, 80, 0.6))',
+    height: '30cm',
+    borderRadius: '10px',
+    backdropFilter: 'blur(10px)',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
   };
 
   return (
-    <div className="container no-padding footer-note">
+    <div className={`container no-padding footer-note ${gridContainerBgStyle}`}>
       <hr className="my-10 border-gray-400 sm:mx-auto dark:border-gray-700 lg:my-7" />
-      <div className="grid grid-cols-bento grid-rows-bento gap-4 md:w-1/2 mx-auto" style={gridStyle}>
+      <div className={`grid ${gridContainerStyle} ${boxShadowStyle}`} style={gridStyle}>
         <BentoGrid>
-          <div className="bg-white hover:bg-gray-200 transition-colors duration-300 flex items-center justify-center text-gray-800 rounded-lg p-4">
-            Grid-
-          </div>
-          <div className="bg-white hover:bg-gray-200 transition-colors duration-300 flex items-center justify-center text-gray-800 rounded-lg p-4">
-            Grid-2
-          </div>
-          <div className="bg-white hover:bg-gray-200 transition-colors duration-300 flex items-center justify-center text-gray-800 rounded-lg p-4">
-            Grid-3
-          </div>
+          <div className={gridItemStyle}>Hello</div>
+          <div className={gridItemStyle}>Hello</div>
+          <div className={gridItemStyle}>Hello</div>
         </BentoGrid>
       </div>
     </div>
