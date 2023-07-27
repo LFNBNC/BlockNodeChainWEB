66974d40c9b693ab95dbe2bb294f52007711c51f
Abin <abinbvinod@gmail.com>
2023-07-27 12:54:34 +0530

    Update bgv.tsx

--- Component/bgv.tsx
+++ Component/bgv.tsx
@@ -12,6 +12,8 @@ const Bgv = () => {
         objectFit: 'cover',
         width: '100%',
         height: '100%',
+        opacity: 0.1, // Set the desired opacity value here (0.0 to 1.0)
+        filter: 'blur(0px)', // Set the desired blur radius here 
       }}>
         <source src="/BF.mp4" type="video/mp4" />
       </video>
